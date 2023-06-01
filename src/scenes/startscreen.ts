76394e1c2b2fd3c051c925fd1ecbfe8e88326c1e
poggersalex <alexander@steeleconsult.com>
2023-06-01 11:00:07 -0600

    More UI Changes, BBYS

--- src/scenes/startscreen.ts
+++ src/scenes/startscreen.ts
@@ -7,11 +7,25 @@ export default class StartScreen extends Phaser.Scene {
         super('start');
     }
     
-    //graphics;
-    bg;
-    startButton
-    startText
-    credBox;
+    colorway = {
+        'bg' : 0x264653,
+        'startBack' : 0xE76F51,
+        'startButton' : 0xF4A261,
+        'credBack' : 0xE9C46A,
+        'credBack2' : 0xE76F51,
+        'backButton' : 0x000000,
+        'backButton2' : 0xFCA311,
+        'credButton1' : 0xE9C46A,
+        'credButton2' : 0xE76F51,
+        'startText' : '264653',
+        'titleText' : 'E76F51',
+        'credLinkText' : '000000',
+        'credText' : '000000',
+        'logoText' : 'E9C46A',
+        
+    }
+
+    // credBox = this.add.group();
 
     init() { }
 
@@ -21,98 +35,95 @@ export default class StartScreen extends Phaser.Scene {
 
         const { width, height } = this.scale;
 
-        var bgRect = new Phaser.Geom.Rectangle(0, 0, width, height)
-        var bg = this.add.graphics({fillStyle: {color : 0x14213D } })
-        bg.fillRoundedRect(bgRect.x, bgRect.y, bgRect.width, bgRect.height, 0);
+        // background
+        this.makeButton(0, 0, width, height, this.colorway['bg'], 0)
     }
+
+    
 
     create() {
         const { width, height } = this.scale;
 
-        var startRect = new Phaser.Geom.Rectangle(width / 2 - 255, height / 2 + 5, 500, 120)
-        
-        var startButtonBack = this.add.graphics({fillStyle : {color : 0xE5E5E5}})
-        startButtonBack.fillRoundedRect(startRect.x - 10, startRect.y - 5, startRect.width, startRect.height, 15)
+        // back of start button
+        this.makeButton(width / 2 - 265, height / 2, 500, 120, this.colorway['startButton'], 15)
 
-        this.startButton = this.add.graphics({fillStyle : {color : 0xFCA311}})
-        this.startButton.fillRoundedRect(startRect.x, startRect.y, startRect.width, startRect.height, 15)
+        // start button
+        this.makeButton(width / 2 - 255, height / 2 + 5, 500, 120, this.colorway['startBack'], 15)
         .setInteractive().on('pointerdown', () => (this.selectPlayers(), console.log('boxclicked')));
 
-        var titleText = this.add.text(width / 2 - 285, height / 2 - 200, 'GAME TITLE', {
-            fontFamily : 'Quicksand', fontSize: '100px', color : '#E5E5E5'
+        // title text
+        this.add.text(width / 2 - 285, height / 2 - 200, 'GAME TITLE', {
+            fontFamily : 'Quicksand', fontSize: '100px', color : '#' + this.colorway['titleText']
         })
 
-
-        this.startText = this.add.text(width / 2 - 225, height / 2 + 32, 'START GAME', {
-            fontFamily: 'Quicksand', fontSize: '70px', color : '#000000'
+        // start text
+        this.add.text(width / 2 - 225, height / 2 + 32, 'START GAME', {
+            fontFamily: 'Quicksand', fontSize: '70px', color : '#' + this.colorway['startText']
         }).setInteractive().on('pointerdown', () => (this.selectPlayers(), console.log('textclicked')));
 
+        // logo text
         this.add.text(30, height - 60, 'INSERT STEELE\n  LOGO HERE', {
-            fontFamily: 'Quicksand', color : '#E5E5E5'
+            fontFamily: 'Quicksand', color : '#' + this.colorway['logoText']
         })
-        this.add.text(width - 165, height - 65, 'CREDITS', {
-            fontFamily : 'Quicksand', fontSize: '30px', color : '#E5E5E5'
+
+        // credit link
+        this.makeButton(width - 210, height - 85, width / 9, height / 15, this.colorway['credButton1'], 15)
+        this.makeButton(width - 200, height - 80, width / 9, height / 15, this.colorway['credButton2'], 15)
+
+        this.add.text(width - 170, height - 65, 'CREDITS', {
+            fontFamily : 'Quicksand', fontSize: '30px', color : '#' + this.colorway['credLinkText']
         }).setInteractive().on('pointerdown', () => (this.openCredits()))
     }
 
     update() { }
 
+
+    // creates a rounded rectangle
+    makeButton(x : number, y : number, w : number, h : number, objColor : number, curve: number){
+        const { width, height } = this.scale;
+
+        var rect = new Phaser.Geom.Rectangle(x, y, w, h)
+        var button = this.add.graphics({fillStyle : {color : objColor}})
+        button.fillRoundedRect(rect.x, rect.y, rect.width, rect.height, curve)
+        return button
+    }
+
+    // changes to players screen
     selectPlayers(){
         this.scene.start('players')
     }
 
-
+    // create credit text box
     openCredits() {
         let {width, height} = this.scale
-        if (this.credBox) {
-            this.credBox.destroy();
-        }
+
+
         
         var credBox = this.add.group();
-        var rect = new Phaser.Geom.Rectangle(width / 4, height / 4, width / 2, height / 2)
-        var credBack = this.add.graphics({ fillStyle: { color : 0xFCA311 } });
-        var credBack2 = this.add.graphics({fillStyle: { color : 0xE5E5E5 } })
-        credBack.fillRoundedRect(rect.x, rect.y, rect.width, rect.height, 30);
-        credBack2.fillRoundedRect(rect.x + 5, rect.y + 5, rect.width, rect.height, 30);
+        credBox.add(this.makeButton(width / 4, height / 4, width / 2, height / 2, this.colorway['credBack2'], 15))
+        credBox.add(this.makeButton(width / 4 + 10, height / 4 + 5, width / 2, height / 2, this.colorway['credBack'], 15))
         
         
-        let creditText = this.add.text(width / 4 + 30, height / 4 + 30, 'Thank you for playing!' 
+        credBox.add(this.add.text(width / 4 + 45, height / 4 + 40, 'Thank you for playing!' 
             + '\n\nCreated By:'
             + '\n\t \tBianka Boudreaux' 
             + '\n\t \tGabriela Sigala Acosta' 
             + '\n\t \tIsmael Parra'
-            + '\n\t \tAlexander Cabos', {fontFamily : 'Quicksand', fontSize : '40px', color : '#000000'})
+            + '\n\t \tAlexander Cabos', {fontFamily : 'Quicksand', fontSize : '40px', color : '#' + this.colorway['credText']}))
 
 
-        
-        var backButton = new Phaser.Geom.Rectangle(width / 2 - 145, height / 2 + 120, width / 6, height / 12)
+        credBox.add(this.makeButton(width / 2 - 145, height / 2 + 120, width / 6, height / 12, this.colorway['backButton'], 15))
+        credBox.add(this.makeButton(width / 2 - 140, height / 2 + 125, width / 6, height / 12, this.colorway['backButton2'], 15))
 
-        var credButton1 = this.add.graphics({ fillStyle: { color: 0xFCA311 } });
-            credButton1.fillRoundedRect(width / 2 - 145, height / 2 + 120, width / 6, height / 12, 15)
-        var credButton2 = this.add.graphics({ fillStyle: { color: 0x14213D } });
-            credButton2.fillRoundedRect(width / 2 - 140, height / 2 + 125, width / 6, height / 12, 15)
-        
-        var credButtonText = this.add.text(width / 2 - 115, height / 2 + 150, 'Back to Menu', {
+        credBox.add(this.add.text(width / 2 - 105, height / 2 + 150, 'Back to Menu', {
             fontFamily : 'Quicksand', fontSize : '30px', color : '#E5E5E5'
-        })
+        }).setInteractive().on('pointerdown', () => this.hideCredBox(credBox)))
         
-        credBox.add(credBack);
-        credBox.add(credBack2);
-        credBox.add(creditText);
-        credBox.add(credButton1);
-        credBox.add(credButton2);
-        credBox.add(credButtonText);
-        
-        
-        credButton1.setInteractive().on('pointerdown', () => this.hideCredBox())
-        credButton2.setInteractive().on('pointerdown', () => this.hideCredBox())
-        credButtonText.setInteractive().on('pointerdown', () => this.hideCredBox())
-        this.credBox = credBox;
     }
      
-    hideCredBox() {
+    hideCredBox(credBox) {
         console.log('hide');
-        this.credBox.destroy(true)
+        credBox.destroy(true)
     }
 }
 
